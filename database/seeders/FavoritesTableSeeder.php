0e7c29333e7d67423c2446d2798f2423ecccba00
syuniki <s.tasaka@gmail.com>
2021-07-11 11:35:36 +0900

    seeder作成

---
+++ database/seeders/FavoritesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Favorite;
+use Illuminate\Database\Seeder;
+
+class FavoritesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 2; $i <= 10; $i++) {
+            Favorite::create([
+                'user_id' => 1,
+                'tweet_id' => $i,
+            ]);
+        }
+    }
+}
