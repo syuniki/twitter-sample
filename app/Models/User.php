dc5de8f3699a396b9f53194937b6a0297b6ede6e
syuniki <s.tasaka@gmail.com>
2021-07-11 10:32:40 +0900

    マイグレーションファイル設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        'screen_name',
         'name',
+        'profile_image',
         'email',
         'password',
     ];
@@ -40,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function followers()
+    {
+        return $this->belongsToMany(self::class, 'followers', 'followed_id', 'following_id');
+    }
+    public function follows()
+    {
+        return $this->belongsToMany(self::class, 'followers', 'following_id', 'followed_id');
+    }
 }
